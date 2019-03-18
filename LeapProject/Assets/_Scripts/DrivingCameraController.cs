f7e83c472cfbc4604c72e97713a016ca405dd008
PhillyDu <philadurose@gmail.com>
2019-03-18 03:30:49 -0500

    Added WheelCollider Scene
    SimpleCarController to control the car with the arrow keys
    DrivingCameraController to control the camera following the car

---
+++ LeapProject/Assets/_Scripts/DrivingCameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingCameraController : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset;
+    public float followSpeed;
+    public float lookSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 lookDirection = objectToFollow.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, lookSpeed * Time.deltaTime);
+    }
+
+    private void MoveToTarget()
+    {
+        Vector3 targetPosition = objectToFollow.position + objectToFollow.forward * offset.z + objectToFollow.right * offset.x + objectToFollow.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+    }
+}
